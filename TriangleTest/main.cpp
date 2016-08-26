a2c1f2565b11004d301bfb57dfc1844ef65977d9
stephen.estell <stephen.estell@SEA-8E3F0D>
2016-08-26 09:19:31 -0700

    k

--- TriangleTest/main.cpp
+++ TriangleTest/main.cpp
@@ -6,10 +6,12 @@ int main()
 {
 	Window window;
 	window.init();
-
+	//clip space coordinates (like a die/ rectangle)
+	//-1,1
+	//1920 x 1080
 	Vertex   vert[3] = { { 0,  .5f,0,1, 1,0,0,1 },
-	{ .5f, -.5f,0,1, 0,1,0,1 },
-	{ -.5f, -.5f,0,1, 0,1,1,1 } };
+					     { .5f, -.5f,0,1, 0,1,0,1 },
+					     { -.5f, -.5f,0,1, 0,1,1,1 } };
 
 	unsigned tris[6] = {0,1,2,3,4,5};
 
